f0832a9f9bb0bc316041e9ad3e614e3459e905a0
anshi06 <aradhya6768@gmail.com>
2024-12-20 10:49:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Machine Coding Practice
+## JEST
+- Javascript Testing Framework
+- testingFunctions folder contains some functions tested by jest
---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="">
+      <img alt="backgroundImage" src="https://cdn.pixabay.com/photo/2024/05/20/13/28/ai-generated-8775232_640.png"/>
+      Machine coding Practice
+    </div>
+  );
+}
+
+export default App;
