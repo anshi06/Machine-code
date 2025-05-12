65a20854e3ede31e5dfce78525993fa830813f75
anshi06 <aradhya6768@gmail.com>
2025-05-12 10:06:46 +0530

    Start

---
+++ src/components/LiveChat/ChatWindow.js
@@ -0,0 +1,17 @@
+import HomeButton from "../HomeButton";
+const ChatWindow = () => {
+    return  <div className="">
+    <div className="flex bg-blue-950 justify-between items-baseline flex-col">
+      <div className="mx-auto">
+        <HomeButton />
+      </div>
+      <h1 className="text-yellow-200 mx-auto font-bold text-3xl mb-6">
+        Live Chat
+      </h1>
+    </div>
+    <div>
+    </div>
+  </div>
+}
+
+export default ChatWindow;
